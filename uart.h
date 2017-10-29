f15fba9413c15166da09de9fa4db8fe12383e2d6
Diptarshi Chakraborty <31807400+DiptarshiC@users.noreply.github.com>
2017-10-30 03:10:55 +0530

    defined function header properly

--- uart.h
+++ uart.h
@@ -1,27 +1,50 @@
 /*
+
  * uart.h
+
  *
- *  Created on: 22-Oct-2017
- *      Author: Anay
+
+ *  Created on: Mar 6, 2017
+
+ *      Author: Diptarshi Chakraborty
+
  */
 
+
+
 #ifndef INCLUDES_UART_H_
+
 #define INCLUDES_UART_H_
+
+
 
 #define SIM_SOPT2_MCGFLLCLK_MASK 0x4000000u
 
+
+
 #define UART0_INT_ENABLE_MASK 0x00001000u
 
-#define Global_Interrupt
+
+
+#define INTERRUPT
+
+
 
 #define OSR_VAL 15
 
 
-void UART_configure(void);
-void UART_send(uint8_t *data);
-void UART_send_n(uint8_t *data_send, uint32_t length);
-void UART_receive(uint8_t *data);
-void UART_receive_n(uint8_t *data_receive, uint32_t length);
-void UART0_IRQHandler();
+
+#define SBR_VAL 0x17
+
+void uart_configure(void);
+
+void uart_send_byte(int8_t data);
+
+void uart_send_byte_n(uint8_t *data, uint32_t length);
+
+int8_t uart_recieve_byte (void);
+
+
 
 #endif /* INCLUDES_UART_H_ */
+
