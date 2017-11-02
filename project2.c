986e7fbe9b015e89732094da9ef6b84f5001c32c
Diptarshi Chakraborty <31807400+DiptarshiC@users.noreply.github.com>
2017-11-02 23:00:33 +0530

    Final touches

--- project2.c
+++ project2.c
@@ -1,173 +1,146 @@
-#include"uart.h"
-#include<stdlib.h>
 #include "cirbuf.h"
 #include "project2.h"
-extern CB_t *REC;
-extern CB_t *SEND;
- CB_status p;
-// uint8_t data;
- int pp=0;
- extern uint8_t alpha;
- extern uint8_t num;
- extern uint8_t punc;
- extern uint8_t misc;
-int main(void)
+#include "uart.h"
+#include "conversion.h"
 
+CB_t * Tx;
+CB_t * Rx;
+int8_t c;
+int8_t alpha = 0, num = 0, punc = 0, misc = 0;
+
+static int i = 0;
+
+int8_t length = 100;
+extern CB_t * Tx;
+extern CB_t * Rx;
+
+void project2()
 {
-	REC=malloc(sizeof(CB_t));
-	REC->buff_point=malloc(sizeof(uint8_t)*100);
-	SEND=malloc(sizeof(CB_t));
-	SEND->buff_point=malloc(sizeof(uint8_t)*100);
-	p=CB_init(REC,100);
-	p=CB_init(SEND,100);
-	//p=CB_buffer_add_item(SEND,50);
-	uart_configure();
+	Tx = malloc(sizeof(CB_t));
+		Tx->buffer = malloc(sizeof(int8_t) * 100);
+		Rx = malloc(sizeof(CB_t));
+		Rx->buffer = malloc(sizeof(int8_t) * 100);
+		CB_init(Rx, length);
+		CB_init(Tx, length);
 
-
-
-while(1)
-	{
-
-	}
+		UART_configure();
 }
 
-/*void count_stats(uint8_t c,uint8_t alpha,uint8_t num,uint8_t punc,uint8_t misc)
-{
-		if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+	void getCount(int8_t c)
+	{
+		UART0_C2 &= ~UART0_C2_TIE_MASK;
+		UART0_C2 &= ~UART0_C2_RIE_MASK;
+		if(c == 13)
+		{
+			printStats();
+		}
+		else if((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		{
 			alpha++;
-		else if(c==33||c==34||c==39||c==44||c==46||c==58||c==59||c==63||c==96)
+		}
+		else if(c >= 48 && c <= 57)	//numeric
+		{
+			num++;
+		}
+		else if(c == 33 || c == 34 || c == 44 || c == 46 || c == 45 || c == 58 || c == 59 || c == 39 || c == 91 || c == 93 || c == 40 || c == 41 || c == 63)
+		{
 			punc++;
-		else if(c>=48&&c<=57)
-				num++;
-		else
-				misc++;
+		}
+		else							//miscellaneous
+		{
+			misc++;
+		}
+	}
 
-
-
-}*/
-void print(uint8_t byte){
-
-	uart_send_byte(byte);
-
-
-}
-
-uint8_t my_itoa (int32_t data, uint8_t * ptr, uint32_t base)
-{
-
-  uint8_t rem;//stores remainder
-  //char c;
-  int i;
-  int32_t temp;			//a temporary variable
-  temp = data;
-
-  uint8_t length;
-  length = 0;
-
-  if (data < 0)			//makes data positive.sign can be added later on
-    data = -(data);
-
-
-  /*The if statements below take out each digit from the number
-     and takes them into pointer/array */
-
-  if (base == 2)
-    {
-      while (data != 0)
+	void printStats()
 	{
-	  rem = data % 2;
-	  *(ptr) = rem + 48;
-	  (ptr++);
-	  length++;
-	  data = data / 2;
+		//Serial.println();
+		char a1[10] = "Alphabets:";
+		int length1 = 9;
+		char * ptr1;
+		ptr1 = &a1[0];
+		for(int i = 0; i < length1; i++)
+		{
+			UART_send(ptr1);
+			ptr1++;
+		}
+
+		uint8_t a2[10];
+		uint8_t * ptr2;
+		ptr2 = &a2[0];
+		ptr2 = my_itoa(alpha, ptr2, 10);
+		uint8_t length2;
+		while(*ptr2 != '\0')
+		{
+			UART_send(ptr2);
+			ptr2++;
+		}
+
+		char a3[8] = " Digits:";
+		int length3 = 13;
+		char * ptr3;
+		ptr3 = &a3[0];
+		for(int i = 0; i < length3; i++)
+		{
+			UART_send(ptr3);
+			ptr3++;
+		}
+
+		uint8_t a4[10];
+		uint8_t * ptr4;
+		ptr4 = &a4[0];
+		ptr4 = my_itoa(num, ptr4, 10);
+		while(*ptr4 != '\0')
+		{
+			UART_send(ptr4);
+			ptr4++;
+		}
+
+		char a5[14] = " Punctuations:";
+		int length5 = 12;
+		char * ptr5;
+		ptr5 = &a5[0];
+		for(int i = 0; i < length5; i++)
+		{
+			UART_send(ptr5);
+			ptr5++;
+		}
+
+		uint8_t a6[10];
+		uint8_t * ptr6;
+		ptr6 = &a6[0];
+		ptr6 = my_itoa(punc, ptr6, 10);
+		while(*ptr6 != '\0')
+		{
+			UART_send(ptr6);
+			ptr6++;
+		}
+
+		char a7[15] = " Miscellaneous:";
+		int length7 = 12;
+		char * ptr7;
+		ptr7 = &a7[0];
+		for(int i = 0; i < length7; i++)
+								{
+									UART_send(ptr7);
+									ptr7++;
+								}
+
+								uint8_t a8[10];
+								uint8_t * ptr8;
+								ptr8 = &a8[0];
+								ptr8 = my_itoa(misc, ptr8, 10);
+								while(*ptr8 != '\0')
+								{
+									UART_send(ptr8);
+									ptr8++;
+								}
+
+		alpha = 0;
+		num = 0;
+		punc = 0;
+		misc = 0;
 	}
-    }
-
-  if (base == 8)
-    {
-      while (data != 0)
-	{
-	  rem = data % 8;
-	  *(ptr) = rem + 48;
-	  (ptr++);
-	  length++;
-	  data = data / 8;
-	}
-    }
-
-  if (base == 10)
-    {
-      while (data != 0)
-	{
-	  rem = data % 10;
-	  *(ptr) = rem + 48;
-	  (ptr++);
-	  length++;
-	  data = data / 10;
-	}
-    }
-
-  if (base == 16)
-    {
-      while (data != 0)
-	{
-	  rem = data % 16;
-	  if (rem == 10)
-	    *ptr = 'A';
-	  else if (rem == 11)
-	    *ptr = 'B';
-	  else if (rem == 12)
-	    *ptr = 'C';
-	  else if (rem == 13)
-	    *ptr = 'D';
-	  else if (rem == 14)
-	    *ptr = 'E';
-	  else if (rem == 15)
-	    *ptr = 'F';
-	  else
-	    *(ptr) = (rem + 48);
-	  (ptr++);
-	  length++;
-	  data = data / 16;
-	}
-    }
-  if (temp < 0)
-    {
-      *ptr = '-';
-      ptr++;
-      length++;
-    }
-  //*ptr='\0';
- ;
-
- int l;
-	if(length%2==0){
-		l=length/2;
-	}
-	else
-		l=(length-1)/2;
-	ptr=ptr-length;
-	for(i=0;i<l;i++)
-	{
-		temp=*(ptr+i);
-		*(ptr+i)=*(ptr+length-i-1);
-		*(ptr+length-i-1)=temp;
-	}
- //ptr=ptr-length;
-  return length;
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- project2.h
+++ project2.h
@@ -1,8 +1,14 @@
-#ifndef INCLUDES_PROJECT2_H_
-#define INCLUDES_PROJECT2_H_
-int main(void);
-void count_stats(uint8_t c,uint8_t alpha,uint8_t num,uint8_t punc,uint8_t misc);
-void print(uint8_t byte);
-uint8_t my_itoa (int32_t data, uint8_t * ptr, uint32_t base);
+#ifndef __PROJECT2_H__
+#define __PROJECT2_H__
 
-#endif /* INCLUDES_PROJECT2_H_ */
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void project2();
+
+void getStatistics(int8_t data);
+
+void printStatistics();
+
+#endif
